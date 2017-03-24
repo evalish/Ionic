8f7c377186c8893e4fadc0436e2fc77efcf948d7
evalish <eldarvalishev@gmail.com>
2017-03-24 18:42:10 +0300

    Exercise (Instructions): Exploring Ionic Part 1

--- assign/www/js/app.js
+++ assign/www/js/app.js
@@ -4,8 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('assign', ['ionic', 'assign.controllers'])
-
+angular.module('assign', ['ionic', 'assign.controllers','assign.services'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
@@ -32,14 +31,19 @@ angular.module('assign', ['ionic', 'assign.controllers'])
     controller: 'AppCtrl'
   })
 
-  .state('app.home', {
+ .state('app.home', {
     url: '/home',
     views: {
       'mainContent': {
-        templateUrl: 'templates/home.html'
+        templateUrl: 'templates/home.html',
+          controller: 'IndexController'
       }
     }
   })
+
+
+
+  
 
   .state('app.aboutus', {
       url: '/aboutus',
